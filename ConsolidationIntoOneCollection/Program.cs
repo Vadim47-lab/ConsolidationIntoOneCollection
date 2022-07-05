787fdcf68ddea1e8f146361e98a2bfb95715d4ab
Stepanov Vadim <vadgat777@mail.ru>
2022-07-05 18:52:19 +0700

    first commit

---
+++ ConsolidationIntoOneCollection/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsolidationIntoOneCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Добро пожаловать в программу: Объединение в одну коллекцию!");
+
+            List<string> firstGroupAthletes = new List<string>{ "Степанов", "Иванов", "Петров", "Сидоров", "Николаев", "Александров", "Владиславов" };
+            List<string> secondGroupAthletes = new List<string> { "Гоголев", "Рубленов", "Сергеев", "Кузнецов", "Николаев", "Александров", "Владиславов" };
+
+            Console.WriteLine("\n Вывод массива первой группы спортсменов = ");
+
+            foreach (var word in firstGroupAthletes)
+            {
+                Console.Write(" " + word + ",");
+            }
+
+            Console.WriteLine("\n\n Вывод массива второй группы спортсменов = ");
+
+            foreach (var word in secondGroupAthletes)
+            {
+                Console.Write(" " + word + ",");
+            }
+
+            for (int i = 0; i < secondGroupAthletes.Count; i++)
+            {
+                if (firstGroupAthletes[i] != secondGroupAthletes[i])
+                {
+                    firstGroupAthletes.Add(secondGroupAthletes[i]);
+                }
+            }
+
+            Console.WriteLine("\n\n Вывод массива спортсменов, которые объединены в одну коллекцию  = ");
+
+            foreach (var word in firstGroupAthletes)
+            {
+                Console.Write(" " + word + ",");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
