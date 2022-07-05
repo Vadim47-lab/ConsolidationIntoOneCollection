29585f77c95b7cec34fb86e223cbbd82a3b391c4
Stepanov Vadim <vadgat777@mail.ru>
2022-07-05 20:00:04 +0700

    second commit

--- ConsolidationIntoOneCollection/Program.cs
+++ ConsolidationIntoOneCollection/Program.cs
@@ -7,41 +7,52 @@ namespace ConsolidationIntoOneCollection
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.CreateCollection();
+        }
+
+        private void CreateCollection()
+        {
             Console.WriteLine(" Добро пожаловать в программу: Объединение в одну коллекцию!");
 
-            List<string> firstGroupAthletes = new List<string>{ "Степанов", "Иванов", "Петров", "Сидоров", "Николаев", "Александров", "Владиславов" };
+            List<string> firstGroupAthletes = new List<string> { "Степанов", "Иванов", "Петров", "Сидоров", "Николаев", "Александров", "Владиславов" };
             List<string> secondGroupAthletes = new List<string> { "Гоголев", "Рубленов", "Сергеев", "Кузнецов", "Николаев", "Александров", "Владиславов" };
+            List<string> listCollection = new List<string>();
 
             Console.WriteLine("\n Вывод массива первой группы спортсменов = ");
 
-            foreach (var word in firstGroupAthletes)
-            {
-                Console.Write(" " + word + ",");
-            }
+            ShowDescription(firstGroupAthletes);
 
             Console.WriteLine("\n\n Вывод массива второй группы спортсменов = ");
 
-            foreach (var word in secondGroupAthletes)
+            ShowDescription(secondGroupAthletes);
+
+            foreach (var word in firstGroupAthletes)
             {
-                Console.Write(" " + word + ",");
+                listCollection.Add(word);
             }
 
             for (int i = 0; i < secondGroupAthletes.Count; i++)
             {
-                if (firstGroupAthletes[i] != secondGroupAthletes[i])
+                if (listCollection[i] != secondGroupAthletes[i])
                 {
-                    firstGroupAthletes.Add(secondGroupAthletes[i]);
+                    listCollection.Add(secondGroupAthletes[i]);
                 }
             }
 
             Console.WriteLine("\n\n Вывод массива спортсменов, которые объединены в одну коллекцию  = ");
 
-            foreach (var word in firstGroupAthletes)
+            ShowDescription(listCollection);
+
+            Console.WriteLine("");
+        }
+
+        private void ShowDescription(List<string> list)
+        {
+            foreach (var word in list)
             {
                 Console.Write(" " + word + ",");
             }
-
-            Console.WriteLine("");
         }
     }
 }
